7d5e1b6ea0c98ecb59828a5832ed131bf3673fdc
Raditya808 <mraditradit808@gmail.com>
2025-09-08 06:39:18 +0700

    tipe data

---
+++ 02.tipe data bentuk type.py
@@ -0,0 +1,81 @@
+# tipe data adalah tipe data yang digunakan untuk menyimpan nilai dalam variabel
+
+# macam macam tipe data
+
+# tipe data angka satuan (integer)
+
+# ini adalah contoh tipe data integer
+# integer adalah tipe data yang digunakan untuk menyimpan bilangan bulat
+data_integer = 1
+print("data: ", data_integer)
+print("- bertipe : ", type(data_integer))
+
+print("==========================")
+
+# ini adalah contoh tipe data float / berkoma
+# tipe data angka dengan koma (float)
+data_float = 1.5
+print("data: ", data_float)
+print("- bertipe : ", type(data_float))
+
+print("==========================")
+
+# tipe data: kumpulan karakter (string)
+data_string = "1.5"
+print("data: ", data_string)
+print("- bertipe : ", type(data_string))
+print("==========================")
+
+# tipe data biner true/false (boolean)
+data_bool = False
+print("data: ", data_bool)
+print("- bertipe : ", type(data_bool))
+print("==========================")
+
+# tipe data khusus
+
+# bilangan kompleks (complex)
+data_bool_complex = complex(5, 6)
+print("data: ", data_bool_complex)
+print("- bertipe : ", type(data_bool_complex))
+print("===========================")
+
+# tipe data bahasa c
+from ctypes import c_double
+from encodings.punycode import T
+from time import process_time_ns
+data_c_double = c_double(1.5)
+print("data: ", data_c_double)
+print("- bertipe : ", type(data_c_double))
+print("=============================================")
+
+# --------------------------------------------
+# macam macam tipe data 
+# kode sendiri
+# kode dalam bentuk integer (int)
+data = 9
+print('data ini adalah',data)
+print('- bertipe :',type(data))
+print("===========================")
+
+# dalam bentuk float
+data2 = 1.5
+print('data ini adalah',data2)
+print('- bertipe :',type(data2))
+print("===========================")
+
+# dalam bentuk string
+data3 = "12"
+print('data ini adalah',data3)
+print('- bertipe :',type(data3))
+print("===========================")
+
+# tipe data boolean true 
+data4 = False
+data5 = True
+print('data ini adalah',data4)
+print('- bertipe :',type(data4))
+print("===========================")
+print('data ini adalah',data5)
+print('- bertipe :',type(data5))
+
