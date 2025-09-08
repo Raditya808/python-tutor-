e59fe312e8dd552810d3aa7256316465b5cdd9eb
Raditya808 <mraditradit808@gmail.com>
2025-09-08 13:21:43 +0700

    Cara python3 bekerja di terminal

---
+++ How programs works with python3/How program and bytecode works.py
@@ -0,0 +1,6 @@
+print("Hello World")
+""" This is a simple Python program that prints "Hello World" to the console."""
+
+'''
+buka terminal lalu ketik python3 maka secara ajaib akan menyuruh pengguna mengetik suatu nilai variabel dan akan ke print
+'''
