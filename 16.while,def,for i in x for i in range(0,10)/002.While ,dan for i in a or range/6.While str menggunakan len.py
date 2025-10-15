2d4f188350f887aebeaf83aa210fc61976bfd498
Raditya808 <mraditradit808@gmail.com>
2025-10-16 06:46:19 +0700

    while len(variabel)

---
+++ 16.while,def,for i in x for i in range(0,10)/002.While ,dan for i in a or range/6.While str menggunakan len.py
@@ -0,0 +1,8 @@
+a = 'radit'
+while len(a)<10: # loop menggunakan len dan selama loop lebih kecil 10 loop akan berjalan  
+    print(a)
+    a = a + '!' # setiap nama radit yang keluar maka akan menambahkan karakter !
+    
+# kenapa output nya hanya berjalan 5 kali ? karena ketika menggunakan len di variabel a 
+# maka akan mengikuti isi dari karakter dari variabel a tersebut dan karena variavbel a 
+# hanya memiliki 5 karakter maka output nya hanya berjalan 5 karakter saja     
