23d806ca7c42571e447898272a875a5237a38d80
Raditya808 <mraditradit808@gmail.com>
2025-10-16 13:10:44 +0700

    function dasar

---
+++ 16.while,def,for i in x for i in range(0,10)/001.def x(args) for i in args/def dasar.py
@@ -0,0 +1,12 @@
+def nama_mahasiswa(nama,umur=0,tujuan='',ktp=92929292929,tempattinggal=''):
+    print('anda:',nama)
+    print('umur anda:',umur)
+    print('tempat tinggal:',tempattinggal)
+    print("ktp: ",ktp)
+    print('kenapa suka python:',tujuan)
+
+nama_mahasiswa('radit',umur=18,tempattinggal='talang bakung',tujuan='python menyenangkan')
+
+'''
+function dasar versi kampus 
+'''
