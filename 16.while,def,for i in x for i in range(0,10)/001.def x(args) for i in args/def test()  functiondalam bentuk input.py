716f5d84e1ccbfeff9ed630d488cfe726eb5bbd7
Raditya808 <mraditradit808@gmail.com>
2025-10-16 20:02:49 +0700

    def test() function input

---
+++ 16.while,def,for i in x for i in range(0,10)/001.def x(args) for i in args/def test()  functiondalam bentuk input.py
@@ -0,0 +1,9 @@
+# def dasar dalam bentuk input 
+# Mendefinisikan (membuat) sebuah fungsi bernama 'test'
+# Tanda kurungnya kosong artinya fungsi ini tidak menerima parameter dari luar
+def test(): 
+    nama = input("masukan nama:")
+    kelas = input("masukan kelas:")
+    print(nama)
+    print(kelas)
+test() # memanggil fungsi
